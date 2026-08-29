382788bee53e3d66969880300898483066e41bae
Sebastian Suarez <261091557+sebastian-suarez@users.noreply.github.com>
2026-08-29 00:03:24 -0500

    [vscodium] 00-telemetry-disable

--- src/vs/workbench/contrib/editTelemetry/browser/editTelemetry.contribution.ts
+++ src/vs/workbench/contrib/editTelemetry/browser/editTelemetry.contribution.ts
@@ -27,7 +27,7 @@ configurationRegistry.registerConfiguration({
 		[EDIT_TELEMETRY_SETTING_ID]: {
 			markdownDescription: localize('telemetry.editStats.enabled', "Controls whether to enable telemetry for edit statistics (only sends statistics if general telemetry is enabled)."),
 			type: 'boolean',
-			default: true,
+			default: false,
 			tags: ['experimental'],
 			experiment: {
 				mode: 'auto'
